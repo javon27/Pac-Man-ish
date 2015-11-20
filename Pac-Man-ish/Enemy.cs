469f136b7546104a8babd39342e22392c5ea0634
Jayson Gallardo <jayson.gallardo@amgreetings.com>
2015-11-20 16:43:11 -0600

    Add Enemy class which inherits from Player class.

---
+++ Pac-Man-ish/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pac_Man_ish
+{
+    class Enemy: Player
+    {
+        public Enemy (char _icon, ConsoleColor color, int x, int y, PlayArea board) 
+            : base (_icon, color, x, y, board)
+        { }
+
+
+    }
+}
