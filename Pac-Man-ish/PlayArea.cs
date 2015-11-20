3a724068a489682f8a0e0d5a38a70923d8d929d8
Jayson Gallardo <jayson.gallardo@amgreetings.com>
2015-11-20 14:51:54 -0600

    initial commit. Second attempt at making windows console game.

---
+++ Pac-Man-ish/PlayArea.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pac_Man_ish
+{
+    class PlayArea
+    {
+        private static string TL = "╔";
+        private static string TR = "╗";
+        private static string BL = "╚";
+        private static string BR = "╝";
+        private static string H = "═";
+        private static string V = "║";
+        public int Width
+        {
+            get; set;
+        }
+        public int Height
+        {
+            get; set;
+        }
+        public object[,] Board
+        {
+            get; private set;
+        }
+        
+        public PlayArea()
+        {
+            Width = Properties.Settings.Default.gameWidth-1;
+            Height = Properties.Settings.Default.gameHeight-4;
+            Board = new object[Width, Height];
+            FillBoard();
+        }
+
+        private void FillBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[,] this[int x, int y]
+        {
+            get { return Board; }
+            set { Board[x, y] = value; }
+        }
+
+        public void Draw()
+        {
+            
+        }
+        
+    }
+}
---
+++ Pac-Man-ish/Player.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pac_Man_ish
+{
+    class Player: IGameObject
+    {        
+        private static int counter = 0;
+        public int Id
+        {
+            get; private set;
+        }
+        public bool Alive
+        {
+            get; set;
+        }
+        public ConsoleColor Color
+        {
+            get; set;
+        }
+        public char Icon
+        {
+            get; set;
+        }
+        public int X
+        {
+            get; set;
+        }
+        public int Y
+        {
+            get; set;
+        }
+        public Vector v
+        {
+            get; set;
+        }
+        private Thread p_thread;
+        public PlayArea board
+        {
+            get; set;
+        }
+        private Drawer drawer;
+
+        public Player(char _icon, int x, int y)
+        {
+            Id = Interlocked.Increment(ref counter);
+            X = x;
+            Y = y;
+            Icon = _icon;
+            Alive = true;
+            Color = ConsoleColor.White;
+            v = Vector.RIGHT;
+            drawer = new Drawer();
+        }
+
+        public void Draw()
+        {
+            try
+            {
+                drawer.DrawObject(this);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                
+            }
+        }
+        
+        private void Move()
+        {
+            do
+            {
+                int x = X;
+                int y = Y;
+                switch (v)
+                {
+                    case Vector.UP:
+                        y = Y - 1;
+                        break;
+                    case Vector.DOWN:
+                        y = Y + 1;
+                        break;
+                    case Vector.LEFT:
+                        x = X - 1;
+                        break;
+                    case Vector.RIGHT:
+                        x = X + 1;
+                        break;
+                }
+                if (board[x, y] != null || x < 0 || x > board.Width || y < 0 || y > board.Height) {
+                    v = Vector.STOP;
+                }
+                else
+                {
+                    X = x;
+                    Y = y;
+                }
+                Thread.Sleep(Game.TICK);
+            } while (Alive);
+        }
+
+        public void Start()
+        {
+            if (p_thread == null)
+            {
+                p_thread = new Thread(Move);
+                p_thread.Start();
+            }
+        }
+
+        ~Player()
+        {
+            Alive = false;
+        }
+    }
+}
