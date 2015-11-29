d8258fb772e383049d4179c46f74967fbfdce4ed
Jayson Gallardo <jayson.gallardo@amgreetings.com>
2015-11-29 01:29:31 -0600

    Add resource file for board layout. Implement loading layout from resource file.

--- Pac-Man-ish/PlayArea.cs
+++ Pac-Man-ish/PlayArea.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,14 @@ namespace Pac_Man_ish
 {
     class PlayArea
     {
-        private static char TL = '╔';
-        private static char TR = '╗';
-        private static char BL = '╚';
-        private static char BR = '╝';
-        private static char H = '═';
-        private static char V = '║';
-        private static char W = '█';
+        private static char[] WALLCHARS =
+        {
+            '╔', '╗', '╚', '╝', '═', '║', '╠', '╣', '╦', '╩'
+        };
+        public static char PELLET = '·';
+        public static char POWERUP = '•';
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
         public int Right
         {
             get; set;
@@ -27,15 +30,19 @@ namespace Pac_Man_ish
         {
             get; private set;
         }
+        public object[,] ActorBoard
+        {
+            get; set;
+        }
         public List<Position> EmptyAreas;
         
         public PlayArea()
         {
             Right = Properties.Settings.Default.windowWidth-2;
-            Bottom = Properties.Settings.Default.windowHeight-5;
-            Board = new object[Right+1, Bottom+1];
+            Bottom = Properties.Settings.Default.windowHeight-4;
+            Board = new object[Right + 1, Bottom + 1];
+            ActorBoard = new object[Right + 1, Bottom + 1];
             FillBoard();
-            GetEmptyAreas();
         }
 
         private void GetEmptyAreas()
@@ -56,40 +63,35 @@ namespace Pac_Man_ish
 
         private void FillBoard()
         {
-            Board[0, 0] = new StationaryObject(TL, ConsoleColor.White, 0, 0);
-            Board[Right, 0] = new StationaryObject(TR, ConsoleColor.White, Right, 0);
-            Board[0, Bottom] = new StationaryObject(BL, ConsoleColor.White, 0, Bottom);
-            Board[Right, Bottom] = new StationaryObject(BR, ConsoleColor.White, Right, Bottom);
-            for (var i = 1; i < Right; i++)
+            List<string> lines = new List<string>();
+            using (Stream stream = assembly.GetManifestResourceStream("Pac_Man_ish.1.brd"))
             {
-                Board[i, 0] = new StationaryObject(H, ConsoleColor.White, i, 0);
-                Board[i, Bottom] = new StationaryObject(H, ConsoleColor.White, i, Bottom);
-            }
-            for (var i = 1; i < Bottom; i++)
-            {
-                Board[0, i] = new StationaryObject(V, ConsoleColor.White, 0, i);
-                Board[Right, i] = new StationaryObject(V, ConsoleColor.White, Right, i);
-            }
-            int[,] walls = new int[,]
-            {
-                {11, 1},
-                {2, 2}, {3, 2}, {4, 2}, {6, 2}, {7, 2}, {8, 2}, {9, 2}, {11, 2}, 
-                {13, 2}, {14, 2}, {15, 2}, {16, 2}, {18, 2}, {19, 2}, {20, 2},
-                {2, 3}, {4, 3}, {6, 3}, {9, 3}, {11, 3}, {13, 3}, {16, 3}, {18, 3}, {20, 3},
-                {2, 4}, {3, 4}, {4, 4}, {6, 4}, {7, 4}, {8, 4}, {9, 4}, {11, 4},
-                {13, 4}, {14, 4}, {15, 4}, {16, 4}, {18, 4}, {19, 4}, {20, 4},
-                {2, 6}, {3, 6}, {4, 6}, {6, 6}, {8, 6}, {9, 6}, {10, 6}, {11, 6},
-                {12, 6}, {13, 6}, {14, 6}, {16, 6}, {18, 6}, {19, 6}, {20, 6},
-                {6, 7}, {11, 7}, {16, 7},
-                {1, 8}, {2, 8}, {3, 8}, {4, 8}, {6, 8}, {7, 8}, {8, 8}, {9, 8}, {11, 8},
-
-            };
-
-            for (var i = 0; i < walls.GetLength(0); i++)
-            {
-                int x = walls[i, 0];
-                int y = walls[i, 1];
-                Board[x, y] = new StationaryObject(W, ConsoleColor.Blue, x, y);
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    int row = 0;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        int col = 0;
+                        foreach (char c in line)
+                        {
+                            if (WALLCHARS.Contains(c))
+                            {
+                                Board[col, row] = new StationaryObject(c, ConsoleColor.Blue, col, row);
+                            }
+                            if (c == PELLET || c == POWERUP)
+                            {
+                                Board[col, row] = new Pellet(c, ConsoleColor.White, col, row);
+                            }
+                            if (c == 'n')
+                            {
+                                Board[col, row] = null;
+                            }
+                            col++;
+                        }
+                        row++;
+                    }
+                }
             }
         }
 
@@ -122,6 +124,42 @@ namespace Pac_Man_ish
             }
         }
         
+    }
+
+    class Pellet : IGameObject
+    {
+        public ConsoleColor Color
+        {
+            get; set;
+        }
+
+        public char Icon
+        {
+            get; set;
+        }
+
+        public int X
+        {
+            get; set;
+        }
+
+        public int Y
+        {
+            get; set;
+        }
+        public bool PowerUp
+        {
+            get; set;
+        }
+        public Pellet(char icon, ConsoleColor color, int x, int y)
+        {
+            Icon = icon;
+            Color = color;
+            X = x;
+            Y = y;
+            if (icon == PlayArea.POWERUP)
+                PowerUp = true;
+        }
     }
 
     class StationaryObject : IGameObject
